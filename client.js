d96d8df23ac8a8ddeb7d132692513fbaed7b550b
Pierre Albaret <pierre.albaret@gmail.com>
2019-09-16 19:37:44 +0200

    MOD: ajout du handshake et memento de l'id du socket

--- client.js
+++ client.js
@@ -1,11 +1,23 @@
 var net = require('net');
 const rl = require('readline');
 
+let state = {
+  handShake: false,
+  socketIdList: [],
+};
+
 var stream = net.createConnection({port: 8000}, () => {
     console.log('connected to server!\r\n');
 });
 
 stream.on('data', (chunk) => {
+    const chunkString = chunk.toString();
+    if(/Your socket id is/.test(chunkString)) {
+        const socketId = chunkString.replace(/Your socket id is :/, '');
+        state.handShake = true;
+        state.socketIdList.push(socketId);
+        console.log('state', state);
+    }
     console.log(`i received ${chunk}\r\n`);
 });
 

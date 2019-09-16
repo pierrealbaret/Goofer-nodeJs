88ae9e078d89ac15a1681b688f80cf9808cdb730
Pierre Albaret <pierre.albaret@gmail.com>
2019-09-16 19:14:01 +0200

    MIN: ajout des retours à la ligne

--- client.js
+++ client.js
@@ -2,15 +2,15 @@ var net = require('net');
 const rl = require('readline');
 
 var stream = net.createConnection({port: 8000}, () => {
-    console.log('connected to server!');
+    console.log('connected to server!\r\n');
 });
 
 stream.on('data', (chunk) => {
-    console.log(`i received ${chunk}`);
+    console.log(`i received ${chunk}\r\n`);
 });
 
 stream.on('end', () => {
-    console.log('fin de la lecture');
+    console.log('fin de la lecture\r\n');
 });
 stream.write('initialize\r\n');
 stream.write("create\r\n");

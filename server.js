0464c249d698023be36ded70018ebc0d8f1237d3
pljarlegan <pl.jarlegan@citae.fr>
2019-09-17 15:31:33 +0200

    SAN: refacto

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const readline = require("readline"),
 
 const server = net.createServer((socket) => {
   "use strict";
+  const endOfResponse = () => { socket.write("end\r\n".cyan); };
+
   socket.id = crypto.randomBytes(16).toString("hex");
   socket.city = null;
   // 'connection' listener
   socket.write("client connected\r\n");
-  socket.write("end\r\n");
+  endOfResponse();
 
 
   socket.on("end", () => {
@@ -27,16 +29,16 @@ const server = net.createServer((socket) => {
     if (line === "initialize") {
       console.log("initialize connexion".blue);
       socket.write(`Your socket id is : ${socket.id} \r\n`.green, "utf-8");
-      socket.write("end\r\n");
+      endOfResponse();
     } else if (line === "create") {
       console.log("create city".blue);
       socket.city = new City(socket, 10, 10);
       console.log("city.map", socket.city.grid);
       socket.city.print();
-      socket.write("end\r\n");
+      endOfResponse();
     } else if (line === "close") {
       console.log("close client".blue);
-      socket.write("end\r\n");
+      endOfResponse();
       socket.end();
     }
 
@@ -44,11 +46,11 @@ const server = net.createServer((socket) => {
       if (line === "populate") {
         console.log("populate goofers".blue);
         socket.city.populate(socket.city.getRandomInt(10), socket.city.width, socket.city.height);
-        socket.write("end\r\n");
+        endOfResponse();
       } else if (line === "print") {
         console.log("print city".blue);
         socket.city.print();
-        socket.write("end\r\n");
+        endOfResponse();
       }
     }
 

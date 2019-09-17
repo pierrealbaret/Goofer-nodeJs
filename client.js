beb5c141ba2d27a32cbd610733ef46eb9c619bcc
pljarlegan <pl.jarlegan@citae.fr>
2019-09-17 14:41:29 +0200

    MOD: avoid send new command before get "end" response

--- client.js
+++ client.js
@@ -1,41 +1,51 @@
 /* eslint-disable no-console */
 const net = require("net"),
-  colors = require("colors"); // eslint-disable-line no-unused-vars
+  colors = require("colors"), // eslint-disable-line no-unused-vars
+  readline = require("readline"),
 
-// rl = require("readline");
-let connected = false;
+  client = net.createConnection({ port: 8000 }, () => {
+    "use strict";
+    console.log("connected to server!".red);
+  });
 
-let commands = [
+let connected = false,
+  readyToSendCommand = false,
+  commands = [
     "initialize",
     "create",
     "populate",
     "print",
     "close",
-  ],
+  ];
 
-  stream = net.createConnection({ port: 8000 }, () => {
-    "use strict";
-    console.log("connected to server!".red);
-  });
-
-stream.on("data", (chunk) => {
+client.on("data", (data) => {
   "use strict";
-  console.log(`i received\r\n${chunk}`);
-  if (chunk.toString() === "client connected\r\n" && !connected) {
+
+  console.log(`Data received : \n${data}`);
+
+  if (data.toString().includes("client connected\r\n") && !connected) {
     connected = true;
+    readyToSendCommand = true;
+    console.log("Server ready to handle commands !".green);
   }
 
-  if (connected) {
+  if (data.toString().includes("end\r\n")) {
+    console.log("readyToSendCommand -> true".gray);
+    readyToSendCommand = true;
+  }
+  if (connected && readyToSendCommand) {
+    readyToSendCommand = false;
     const command = commands.shift();
     if (command) {
-      console.log(`Send command ${command}`.blue);
-      stream.write(`${command}\r\n`);
+      console.log("Send command", command.blue);
+      client.write(`${command}\r\n`);
     }
   }
+
 });
 
-stream.on("end", () => {
+client.on("end", () => {
   "use strict";
-  console.log("fin de la lecture".red);
+  console.log("disconnected from server".red);
 });
 

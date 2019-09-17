b634116b16dbe98a57c2bf14fd725572d31da86b
pljarlegan <pl.jarlegan@citae.fr>
2019-09-17 09:58:05 +0200

    SAN: add eslint

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
-const net = require('net');
-const rl = require('readline');
+const net = require("net"),
+  rl = require("readline");
 let connected = false;
 
 const commands = [
@@ -7,30 +7,28 @@ const commands = [
     "create\r\n",
 
     "populate\r\n",
-    "print\r\n"
+    "print\r\n",
 
-];
-const stream = net.createConnection({ port: 8000 }, () => {
-    console.log('connected to server!');
+  ],
+  stream = net.createConnection({ port: 8000 }, () => {
+    console.log("connected to server!");
+  });
+
+stream.on("data", (chunk) => {
+  console.log(`i received\r\n${chunk}`);
+  if (chunk.toString() === "client connected\r\n" && !connected) {
+    connected = true;
+  }
+  if (connected) {
+    commands.map((command) => {
+      console.log(command);
+      stream.write(command);
+      commands.shift();
+    });
+  }
 });
 
-stream.on('data', (chunk) => {
-    console.log(`i received\r\n${chunk}`);
-    if (chunk.toString() === "client connected\r\n" && !connected) {
-        connected = true;
-    }
-    if (connected) {
-        commands.map((command) => {
-            console.log(command);
-            stream.write(command);
-            commands.shift();
-        });
-    }
+stream.on("end", () => {
+  console.log("fin de la lecture");
 });
-
-stream.on('end', () => {
-    console.log('fin de la lecture');
-});
-
-
 
--- server.js
+++ server.js
@@ -1,55 +1,51 @@
-const readline = require("readline");
-const net = require('net');
-const crypto = require('crypto');
+const readline = require("readline"),
+  net = require("net"),
+  crypto = require("crypto"),
 
-
-
-
-
-const City = require('./city');
+  City = require("./city");
 
 const server = net.createServer((socket) => {
-    socket.id = crypto.randomBytes(16).toString('hex');
-    socket.city = null;
-    // 'connection' listener
-    socket.write('client connected\r\n');
-    socket.on('end', () => {
-        console.log('client disconnected');
-    });
+  socket.id = crypto.randomBytes(16).toString("hex");
+  socket.city = null;
+  // 'connection' listener
+  socket.write("client connected\r\n");
+  socket.on("end", () => {
+    console.log("client disconnected");
+  });
 
-    const city = new City(10, 10, socket);
-    console.log('city.map', city.grid);
+  const city = new City(10, 10, socket);
+  console.log("city.map", city.grid);
 
-    socket.write(`hello \r\n`, 'utf-8');
-    socket.write(`${socket.id} \r\n`, 'utf-8');
+  socket.write("hello \r\n", "utf-8");
+  socket.write(`${socket.id} \r\n`, "utf-8");
 
-    const rl = readline.createInterface(socket);
+  const rl = readline.createInterface(socket);
 
-    rl.on("line", function(line) {
-        console.log('retrieving a line', line, line === 'initialize ', typeof line);
-        if(line === 'initialize') {
-            console.log('write ID SOCKET');
-            socket.write(`Your socket id is : ${socket.id} \r\n`, 'utf-8');
-        } else if(line === 'create') {
-            city.print();
-        } else if(line === 'populate') {
-            city.populate();
-        } else if(line === 'print') {
-            city.print();
-        }
-    });
-    rl.on('close', function() {
-        console.log('closing stream');
-    });
+  rl.on("line", (line) => {
+    console.log("retrieving a line", line, line === "initialize ", typeof line);
+    if (line === "initialize") {
+      console.log("write ID SOCKET");
+      socket.write(`Your socket id is : ${socket.id} \r\n`, "utf-8");
+    } else if (line === "create") {
+      city.print();
+    } else if (line === "populate") {
+      city.populate();
+    } else if (line === "print") {
+      city.print();
+    }
+  });
+  rl.on("close", () => {
+    console.log("closing stream");
+  });
 
-    // socket.end();
+  // socket.end();
 });
 
-server.on('error', (err) => {
-    throw err;
+server.on("error", (err) => {
+  throw err;
 });
 
 server.listen(8000, () => {
-    console.log('server bound');
+  console.log("server bound");
 });
 

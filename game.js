c121bbd9b0e4211ff382709d48ebb2b0a37ac1ff
pljarlegan <pl.jarlegan@citae.fr>
2019-09-26 17:07:18 +0200

    SAN: clean

--- game.js
+++ game.js
@@ -12,7 +12,6 @@ module.exports = class Game {
   }
 
   processCommands() {
-    console.log("¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤PROCESS COMMANDS");
     console.log(this.commands.length, this.nbPlayers);
     if(this.city && this.commands.length === this.nbPlayers) {
       while( this.commands.length > 0) {
@@ -20,9 +19,6 @@ module.exports = class Game {
         const command = this.commands[ itemIndex ];
         this.commands.splice(itemIndex);
         this.process(command);
-        console.log("¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤commandes restantes");
-        console.log(this.commands.length);
-        console.log("¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤");
       }
       Object.values(this.city.players).forEach((player) => {
         this.city.print(player.id);
@@ -32,16 +28,11 @@ module.exports = class Game {
   }
 
   process(command) {
-    console.log("¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ PROCESS");
-    console.log(command);
-    console.log("¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤");
     const params = command.command.match(/^move ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)/);
     this.city.move(
       command.playerId,
       { x: parseInt(params[ 1 ]), y: parseInt(params[ 2 ]) },
     { x: parseInt(params[ 3 ]), y: parseInt(params[ 4 ]) }
         );
-
-    console.log("¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤");
   }
 };

4e91c3ad5428259b15edc70c94677b2898126b34
pljarlegan <pl.jarlegan@citae.fr>
2019-10-04 15:38:40 +0200

    MIN: tmp

--- commandHandlers/print.js
+++ commandHandlers/print.js
@@ -6,7 +6,14 @@ const
       currentGame = games.find((game) => game.id === socket.gameId);
     if (currentGame !== null) {
       console.log("print city".blue); // eslint-disable-line no-console
-      currentGame.city.print(socket.id);
+      // currentGame.city.print(socket.id); // TODO
+
+      // TEST ALEX
+      socket.emit("print", [
+        [ { item: "?" }, { item: "." }, { item: "?" }, { item: "." } ],
+        [ { item: "G", player: "id1" }, { item: "R" }, { item: "R" }, { item: "." } ],
+        [ { item: "." }, { item: "G", player: "id2" }, { item: "R" }, { item: "R" } ],
+      ]);
     }
   };
 

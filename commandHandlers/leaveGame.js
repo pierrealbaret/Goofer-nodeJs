1ef11c8324a459b67dfbd034d3030f3263a32fae
dokoliday <dokoliday@hotmail.fr>
2019-10-11 16:29:27 +0200

    ADD : command leaveGame call new print grid for all players

--- commandHandlers/leaveGame.js
+++ commandHandlers/leaveGame.js
@@ -1,14 +1,18 @@
 const
-  isPlayerInGame = require("../helpers/isPlayerInGame"),
-  handler = (parameters) => {
-    const { games,socket} = parameters;
-    if (isPlayerInGame(games, socket.id) === true) {
-        currentGame=games.find(element=>element.id===socket.gameId);
-        delete currentGame.city.players[socket.id];
-    }}
+    isPlayerInGame = require("../helpers/isPlayerInGame"),
+    handler = (parameters) => {
+        const { games, socket } = parameters;
+        if (isPlayerInGame(games, socket.id) === true) {
+            currentGame = games.find(element => element.id === socket.gameId);
+            delete currentGame.city.players[socket.id];
+            socket.gameId = "";
+            Object.values(currentGame.city.players)
+                .map(player=>currentGame.city.print(player.id))
+        }
+    };
 
 module.exports = {
-  name: "leaveGame",
-  description: "leave game",
-  handler: handler,
+    name: "leaveGame",
+    description: "leave game",
+    handler: handler,
 };
